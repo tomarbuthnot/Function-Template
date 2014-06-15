4ca014857b49cf1cf8ca1dc8b1642399f3098507
Tom Arbuthnot <arbuthnot@outlook.com>
2014-06-15 15:59:45 +0100

    minor formatting

--- Function-Template.ps1
+++ Function-Template.ps1
@@ -6,43 +6,67 @@ Function Function-Template {
 .DESCRIPTION  
 		Description goes here
 
+.LINK  
+    http://link.com
+                
 .NOTES  
-    Version							: 0.x
-		
-    Ideas/Wish list			: 
+    Version:		        0.x
+  
+    Author/Copyright:	  Copyright Tom Arbuthnot - All Rights Reserved
     
-    Rights Required			: 
+    Email/Blog/Twitter:	tom@tomarbuthnot.com tomarbuthnot.com @tomarbuthnot
     
-    Sched Task Required	: No
-    
-    Author/Copyright		: © Tom Arbuthnot - All Rights Reserved
-    
-    Email/Blog/Twitter	: tom@tomarbuthnot.com lyncdup.com @tomarbuthnot
-    
-    Dedicated Post			: 
-    
-    Disclaimer   				: THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK
-                          OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
-                          While these scripts are tested and working in my environment, it is recommended 
-                          that you test these scripts in a test environment before using in your production 
-                          environment
-                          Tom Arbuthnot further disclaims all implied warranties including, without limitation, any 
-                          implied warranties of merchantability or of fitness for a particular purpose. The entire risk 
-                          arising out of the use or performance of this script and documentation remains with you. 
-                          In no event shall Tom Arbuthnot, its authors, or anyone else involved in the creation, production, 
-                          or delivery of this script/tool be liable for any damages whatsoever (including, without limitation, 
-                          damages for loss of business profits, business interruption, loss of business information, or other 
-                          pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, 
-                          even if Tom Arbuthnot has been advised of the possibility of such damages
+    Disclaimer:   	    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK
+                        OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+                        While these scripts are tested and working in my environment, it is recommended 
+                        that you test these scripts in a test environment before using in your production 
+                        environment. Tom Arbuthnot further disclaims all implied warranties including, 
+			                  without limitation, any implied warranties of merchantability or of fitness for 
+			                  a particular purpose. The entire risk arising out of the use or performance of 
+			                  this script and documentation remains with you. In no event shall Tom Arbuthnot, 
+			                  its authors, or anyone else involved in the creation, production, or delivery of 
+			                  this script/tool be liable for any damages whatsoever (including, without limitation, 
+                        damages for loss of business profits, business interruption, loss of business 
+			                  information, or other pecuniary loss) arising out of the use of or inability to use 
+			                  the sample scripts or documentation, even if Tom Arbuthnot has been advised of 
+			                  the possibility of such damages.
     
      
-    Acknowledgements 		: 
+    Acknowledgements: 	
     
-    Assumptions					: ExecutionPolicy of AllSigned (recommended), RemoteSigned or Unrestricted (not recommended)
+    Assumptions:	      ExecutionPolicy of AllSigned (recommended), RemoteSigned or Unrestricted (not recommended)
     
-    Limitations					:    										
+    Limitations:		  										
+    		
+    Ideas/Wish list:	
     
-    Known issues				: 
+    Rights Required:	
+
+    Known issues:	
+
+
+.EXAMPLE
+		Function-Template
+ 
+		Description
+		-----------
+		Returns Objects
+
+.EXAMPLE
+		Function-Template -Param1
+ 
+		Description
+		-----------
+		Actions Param1
+
+# Parameters
+
+.PARAMETER Param1
+		Param1 description
+
+.PARAMETER Param2
+		Param2 Description
+		
 
 #>
   
@@ -139,7 +163,7 @@ Function Function-Template {
       Catch 	{ErrorCatch-Action} # Close Catch Block
       
       
-    } # Close If Everthing OK Block
+    } # Close If EverthingOK Block 1
     
     #############################################################
     # Next Script Action or Try,Catch Block
@@ -159,10 +183,10 @@ Function Function-Template {
       Catch 	{ErrorCatch-Action} # Close Catch Block
       
       
-    } # Close If Everthing OK Block
+    } # Close If EverthingOK Block 2
     
     
-  } #Close Function Process Block
+  } #Close Function Process Block 
   
   #############################################################
   # End Block
